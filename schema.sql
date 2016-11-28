6cd6b1fc6db55f23496e3c80565864d20d1b5e6d
XUEQI FAN <xueqi.fan@mail.utoronto.ca>
2016-11-28 14:28:29 -0500

    change schema

--- schema.sql
+++ schema.sql
@@ -5,6 +5,8 @@ CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   username TEXT NOT NULL,
   password TEXT NOT NULL,
+  email TEXT,
+  phone TEXT,
   is_admin INTEGER DEFAULT 0
 );
 
@@ -12,13 +14,13 @@ DROP TABLE IF EXISTS goods;
 CREATE TABLE goods (
   id INTEGER PRIMARY KEY,
   user_id INT,
-  name TEXT NOT NULL,
+  name TEXT,
   price INT NOT NULL,
-  tag VARCHAR(10) NOT NULL CHECK (tag in ('XS','S','M','L','XL')),
-  size VARCHAR(2) NOT NULL CHECK (size in ('top', 'bottom', 'dress', 'other')),
+  size VARCHAR(10) NOT NULL CHECK (size in ('XS','S','M','L','XL')),
+  tag VARCHAR(2) NOT NULL CHECK (tag in ('Top', 'Bottom', 'Dress', 'Other')),
   description TEXT,
-  comment VARCHAR(128),
-  timestamp INTEGER NOT NULL DEFAULT (datetime('now')),
+  picture TEXT,
+  comments TEXT,
 
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
@@ -27,3 +29,13 @@ INSERT INTO users (username, password, is_admin) VALUES ('xueqi', '$2a$10$CiNxJ.
 INSERT INTO users (username, password, is_admin) VALUES ('minhua', '$2a$10$Y5GmdCeDbjEOaShJ.sPWwe5rtYvAPgEzIgRZXP.oU.43UuYMeATlu', 1);
 INSERT INTO users (username, password, is_admin) VALUES ('zhujun', '$2a$10$9u5ozAyJWkmZnJ4rSHhRlOXupPI144NP3px0uGboVAMH4oPQLvkmO', 1);
 INSERT INTO users (username, password, is_admin) VALUES ('xinyan', '$2a$10$CiNxJ.OtQc9/DKiRX79Z3OfglNcAhDC9GCxbxovOCA0JSkjxvV9eS', 1);
+
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('A', '40', 'Top', 'XS', 'item', 'ABC');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('B', '40', 'Bottom', 'S', 'item', 'DEF');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('C', '140', 'Dress', 'M', 'item', 'ABC');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('D', '140', 'Top', 'L', 'item','DEF');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('E', '240', 'Other', 'XL', 'item', 'GHI');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('F', '240', 'Dress', 'XS', 'item', 'ABC');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('G', '340', 'Top', 'S', 'item', 'GHI');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('H', '440', 'Bottom', 'M', 'item', 'DEF');
+INSERT INTO goods (name, price, tag, size, picture, description) VALUES ('I', '540', 'Other', 'XL', 'item', 'ABCDEF');
