c4ad48d072aafa121b4173f3218de515fc364793
Minhua Zhu <minhua.zhu@mail.utornoto.ca>
2016-12-03 17:11:50 -0500

    usrjnfo js

--- static/js/userinfo.js
+++ static/js/userinfo.js
@@ -160,15 +160,12 @@ function loadMsg(msgs){
 
 	if (msgs.length > 0){
 	tableR = "";
-	tableR += "<table  class=\"table table-bordered  table-hover\">";
-    tableR += "<tr class='warning'><td  align='center'>name</td><td align='center'>message</td><td align='center'>Reply</td>"
-                       + "</tr>";
+	// tableR += "<table  class=\"table table-bordered  table-hover\">";
+ //    tableR += "<tr class='warning'><td  align='center'>name</td><td align='center'>message</td><td align='center'>Reply</td>"
+ //                       + "</tr>";
 	for (let i = 0; i < msgs.length; i ++){
 		tableR = tableR + "<tr>" + "<td align='center'>" + msgs[i].name + "</td><td align='center'>"
 		        + msgs[i].message + "</td><td align='center'>";
-		// let div = "<div class='row'>";
-		// let name = "<h4 class='list-group-item-heading col-md-4'>"+msgs[i].name+"</h4>";
-		// let message = "<p class='list-group-item-text col-md-4'>"+msgs[i].message+"</p>";
 		let button = "<button class='btn btn-default btn-sm'><a href='/send?receiverName=" + msgs[i].name + "'> Reply</a></button>";
 		tableR = tableR + button + "</td>" + "</tr>";
 		// re = re + div + name + message + button + "</div>";
