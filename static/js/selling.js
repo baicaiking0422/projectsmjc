8bc5a063fb935bdcfd7bac7c60ff100067da63d9
XUEQI FAN <xueqi.fan@mail.utoronto.ca>
2016-12-03 14:21:13 -0500

    add message page and link pages together

--- static/js/selling.js
+++ static/js/selling.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 	refresh_comments();
+	var signInid = $("#buythis").attr("name");
+	//console.log(signInid);
+	$('#basesigninSuc').attr("action", "/signin?good_id="+signInid);
 
 	$("#buythis").click(function(){
 		var id = $(this).attr("name");
@@ -8,11 +11,48 @@ $(document).ready(function() {
 			url: "/buy?item_id=" + id,
 			type: "POST",
 			success: function(response){
-				console.log(response);
+				//console.log(response);
 				if (response == "Not Reg") {
 					console.log("not reg");
 					// TODO: 弹窗
+					$('#basesigninSuc').attr("action", "/signin?item_id="+id);
+					
+					$('#baseSignin').click();
 				}
+				if (response == "Reg and Bought") {
+					console.log("reg and bought");
+					console.log(location.protocol);
+					console.log(location.pathname);
+					console.log(location.hostname);
+					window.location = '/';
+				}
+
+			}
+		});
+	});
+
+	$('#sellingChat').click(function(){
+		var item_id = $(this).attr("name");
+		var id = $('#selling').attr("name");
+		//console.log("item_id is " + item_id + " id is " + id);
+		$.ajax({
+			url: "/sendmessage?sender=" + id + "&item_id=" + item_id,
+			type: "GET",
+			success: function(response){
+				//console.log("send response " + response);
+				if (response == "Not Reg") {
+					console.log("not reg");
+					// TODO: 弹窗
+					$('#basesigninSuc').attr("action", "/signin?item_id="+item_id);
+					
+					$('#baseSignin').click();
+				}else{
+					var res = JSON.parse(response);
+					//console.log('res is ' + response);
+					var receiver = res.receiver;
+					window.location = '/send?receiver='+ receiver;
+				}
+
 			}
 		});
 	});
